678aed85aef8cc2cb91b74bb163573ba1b0cfb18
Bernard <kakengi_bernard@hotmail.com>
2018-06-18 23:41:55 +0300

    update files

--- palindrome.js
+++ palindrome.js
@@ -8,3 +8,27 @@ function palindrome(string) {
   let processedContent = string.toLowerCase();
   return processedContent === reverse(processedContent);
 }
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    let processedContent = this.content.toLowerCase();
+    return processedContent === reverse(processedContent);
+  }
+}
+/*
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Makes the phrase LOUDER.
+  this.louder = function() {
+  	let upperCaps = this.content.toUpperCase();
+  	console.log(upperCaps);
+    // FILL IN
+  };
+}
+*/
